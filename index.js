865c40fb5f8567db4c22b2bd0ef4cf816685bf04
Jkomba <johnkombah88@gmail.com>
2023-09-15 21:35:07 +0300

    done with assignment but not able to pass all tests

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+  
